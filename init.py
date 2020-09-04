8591e2fa6292da8707af7ce6a74d4c1deda5de64
rlx <rramosp@unal.edu.co>
2020-09-04 14:21:25 -0500

    init

--- init.py
+++ init.py
@@ -1,6 +1,9 @@
 course_id = 'EACD-05-SEMINARIO'
 github_repo = 'UDEA-Esp-Analitica-y-Ciencia-de-Datos/%s'%course_id
 zip_file_url="https://github.com/%s/archive/master.zip"%github_repo
+endpoint = 'https://m5knaekxo6.execute-api.us-west-2.amazonaws.com/dev-v0001/rlxmooc'
+#endpoint = 'http://localhost:5000/rlxmooc'
+
 
 def get_last_modif_date(localdir):
     try:
@@ -13,8 +16,8 @@ def get_last_modif_date(localdir):
     except Exception:
         return None
     
-import requests, zipfile, io, os, shutil
 def init(force_download=False):
+
     if force_download or not os.path.exists("local"):
         print("replicating local resources")
         dirname = course_id+"-master/"
@@ -27,4 +30,29 @@ def init(force_download=False):
             shutil.rmtree("local")
         shutil.move(dirname+"/local", "local")
         shutil.rmtree(dirname)
+
+def get_weblink():
+    from IPython.display import HTML
+    print ("endpoint", endpoint)
+    return HTML("<h3>See <a href='"+endpoint+"/web/login' target='_blank'>my courses and progress</a></h2>")
+
+def install_sourcedefender():
+    print('enabling encryption...')
+    output = subprocess.run(['pip', 'install', 'sourcedefender'], stderr=subprocess.PIPE)
+
+    if output.returncode != 0:
+        STDOUT_RED_COLOR = '\033[91m'
+        STDOUT_RESET_COLOR = '\033[0m'
+        print('Sourcedefender installation failed, returning')
+        print(STDOUT_RED_COLOR + output.stderr.decode('ASCII') + STDOUT_RESET_COLOR)
+    else:
+       print('encryption enabled')
+
+
+import requests, zipfile, io, os, shutil, subprocess
+try:
+    import sourcedefender
+except ModuleNotFoundError:
+    install_sourcedefender()
+    import sourcedefender
 
